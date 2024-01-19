7f91667ebbbd21f975c8066d12fe00d1c534be2e
stefanrogc <stefanrogic@protonmail.com>
2024-01-19 14:00:18 +0100

    added sign up modal

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,10 @@
+// BUTTONS
+const signInBtn = document.getElementById("signin-btn") as HTMLButtonElement | null;
 
+// MODALS
+const signInModal = document.getElementById("signin-modal") as HTMLDivElement | null;
+
+// EVENT LISTENERS
+signInBtn?.addEventListener("click", () => signInModal?.classList.toggle("hidden"));
+signInModal?.firstElementChild?.addEventListener("click", () => signInModal?.classList.add("hidden"));
+signInModal?.lastElementChild?.firstElementChild?.addEventListener("click", () => signInModal?.classList.add("hidden"));

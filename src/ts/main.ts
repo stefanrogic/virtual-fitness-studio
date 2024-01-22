360d7290bfb49b8276278460f5a87558f9231d51
stefanrogc <stefanrogic@protonmail.com>
2024-01-22 19:50:22 +0100

    tweaked hero section and fixed element stacking

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,10 +1,38 @@
+let navToggle: boolean = false;
+let modalToggle: boolean = false;
+
 // BUTTONS
 const signInBtn = document.getElementById("signin-btn") as HTMLButtonElement | null;
 
 // MODALS
 const signInModal = document.getElementById("signin-modal") as HTMLDivElement | null;
 
+// NAVBAR
+const navbar = document.getElementById("navbar") as HTMLDivElement | null;
+
 // EVENT LISTENERS
-signInBtn?.addEventListener("click", () => signInModal?.classList.toggle("hidden"));
-signInModal?.firstElementChild?.addEventListener("click", () => signInModal?.classList.add("hidden"));
-signInModal?.lastElementChild?.firstElementChild?.addEventListener("click", () => signInModal?.classList.add("hidden"));
+signInBtn?.addEventListener("click", () => {
+  signInModal?.classList.remove("hidden");
+  navbar?.classList.remove("sticky");
+  modalToggle = true;
+});
+signInModal?.firstElementChild?.addEventListener("click", () => {
+  signInModal?.classList.add("hidden");
+  modalToggle = false;
+});
+signInModal?.lastElementChild?.firstElementChild?.addEventListener("click", () => {
+  signInModal?.classList.add("hidden");
+  modalToggle = false;
+});
+
+window.addEventListener("mousemove", (e: MouseEvent) => {
+  if (e.clientY <= 89 && !navToggle && !modalToggle) {
+    navbar?.classList.add("sticky");
+    navToggle = true;
+  }
+
+  if (e.clientY > 89 && navToggle) {
+    navbar?.classList.remove("sticky");
+    navToggle = false;
+  }
+});
